5c29046d4c0042aafcda440c442b1290d8f2f45e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-05-19 15:26:26 +0200

    Initial revision.
    This project is a very simple (likely to be transient) demonstration of
    the new luminance context / state tracker systems.

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+extern crate luminance;
+
+use luminance::context::{GraphicsContext, WithGraphicsState, thread_acquire_context};
+use luminance::state::GraphicsState;
+
+pub struct DummyContext {
+  graphics_state: GraphicsState
+}
+
+impl DummyContext {
+  pub fn new() -> Option<Self> {
+    thread_acquire_context(DummyContextBuilder)
+  }
+}
+
+unsafe impl GraphicsContext for DummyContext {
+  fn graphics_state(&mut self) -> &mut GraphicsState {
+    &mut self.graphics_state
+  }
+
+  fn swap_buffers(&mut self) {
+    println!("swapping buffers!");
+  }
+}
+
+struct DummyContextBuilder;
+
+impl WithGraphicsState for DummyContextBuilder {
+  type Output = DummyContext;
+
+  fn call_once<F>(self, gfx_state: F) -> Self::Output where F: FnOnce() -> GraphicsState {
+    DummyContext { graphics_state: gfx_state() }
+  }
+}
