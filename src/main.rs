9e7d2570b0cff9c46684528c1650277542ba68ef
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-05-31 01:26:46 +0200

    It seems to work correctly! Next test: spectra and a demo!

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,32 @@ extern crate luminance;
 extern crate luminance_dummy;
 extern crate luminance_glfw;
 
-use luminance_glfw::events::{Action, Key, WindowEvent};
+use luminance::context::GraphicsContext;
+use luminance::framebuffer::Framebuffer;
+use luminance_glfw::event::{Action, Key, WindowEvent};
 use luminance_glfw::surface::{GlfwSurface, Surface, WindowDim, WindowOpt};
-use std::sync::mpsc;
-use std::thread;
-
-enum Cmd {
-  Quit
-}
 
 fn main() {
-  let (mut surface, mut events) = GlfwSurface::new(WindowDim::Windowed(800, 600), "test", WindowOpt::default()).expect("window::new");
-
-  // channel between event loop and render loop
-  let (cmd_sx, cmd_rx) = mpsc::channel();
-  // event loop
-  thread::spawn(move || {
-    loop {
-      for event in events.wait_iter() {
-        match event {
-          WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => {
-            let _ = cmd_sx.send(Cmd::Quit);
-          }
-          _ => ()
-        }
-      }
-    }
-  });
+  let mut surface = GlfwSurface::new(WindowDim::Windowed(800, 600), "test", WindowOpt::default()).expect("window::new");
+  let mut screen_color = [0., 0., 0., 1.];
 
   // render and main loop
   'app: loop {
-    // exit if the event loop asks to
-    if let Ok(cmd) = cmd_rx.try_recv() {
-      match cmd {
-        Cmd::Quit => break 'app
+    for event in surface.poll_events() {
+      match event {
+        WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => break 'app,
+        WindowEvent::Key(Key::R, _, Action::Release, _) => screen_color = [1., 0., 0., 1.],
+        WindowEvent::Key(Key::G, _, Action::Release, _) => screen_color = [0., 1., 0., 1.],
+        WindowEvent::Key(Key::B, _, Action::Release, _) => screen_color = [0., 0., 1., 1.],
+        _ => ()
       }
     }
 
-    surface.draw(|| {
-      println!("render!");
+    let builder = surface.pipeline_builder();
+    let screen = Framebuffer::default(surface.size());
+    builder.pipeline(&screen, screen_color, |_, _| {
     });
+
+    surface.swap_buffers();
   }
 }
